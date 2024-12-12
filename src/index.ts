4e09a9e44332cc38c18879874dc5a3e569db98e9
AHMZ <ahmz.ut@gmail.com>
2024-12-12 19:31:50 +0330

    Come back to http

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import https from 'https';
-import * as fs from "node:fs";
+import http from 'http';
+// import * as fs from "node:fs";
 
 dotenv.config(); // Load environment variables
 const PORT = process.env.PORT || 3000;
-const options = {
-    key: fs.readFileSync('privateKey.key'),
-    cert: fs.readFileSync('certificate.crt')
-};
+// const options = {
+//     key: fs.readFileSync('privateKey.key'),
+//     cert: fs.readFileSync('certificate.crt')
+// };
 const app = express();
 
 app.use(express.json());
@@ -17,4 +17,4 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-https.createServer(options, app).listen(PORT);
+http.createServer(app).listen(PORT);
