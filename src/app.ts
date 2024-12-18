7dbcb02d68d03d71232d5d8b66043f03795a07ef
AHMZ <ahmz.ut@gmail.com>
2024-12-18 17:40:28 +0330

    Revert Change routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,12 @@ import {json} from "./middleware/validator";
 import routesV1 from "./routes/v1/index";
 import {errorHandler, NotFoundError} from "./utils/errors";
 import authLimiter from "./utils/ratelimiter";
-import userRoutes from "./routes/v1/userRoutes";
 
 const app = express();
 
-// Middleware to parse JSON and URL-encoded requests
 app.use(json);
 app.use(express.urlencoded({extended: true}));
 
-// Default API root route
 app.get("/", (_req, res) => {
     res.send("Welcome to QuizHive API. Please refer to the documentation for proper usage");
 });
@@ -23,16 +20,8 @@ if (config.env === "production") {
     app.use("/api/v1/auth", authLimiter);
 }
 
-// Add user routes explicitly
-app.use("/api/v1/users", userRoutes);
-
-// Mount version 1 API routes
 app.use("/api/v1/", routesV1);
-
-// Catch-all route for undefined endpoints
 app.use((_req, _res, next) => next(new NotFoundError()));
-
-// Global error handler
 app.use(errorHandler);
 
 export default app;
